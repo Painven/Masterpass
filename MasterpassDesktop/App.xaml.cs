04d468cdac0443d67f4e873a12f8d7c49bd4301f
Дмитрий Смирнов <painven@gmail.com>
2023-02-16 00:28:21 +0300

    Add project files.

---
+++ MasterpassDesktop/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MasterpassDesktop.ViewModels;
+using System.Windows;
+
+namespace MasterpassDesktop;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var vm = new MainWindowViewModel();
+        var mainWindow = new MainWindow();
+        mainWindow.DataContext = vm;
+        mainWindow.Show();
+    }
+}
---
+++ MasterpassDesktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MasterpassDesktop.ViewModels;
+public class MainWindowViewModel : ViewModelBase
+{
+    public ICommand CustomCommand { get; }
+    public MainWindowViewModel()
+    {
+        CustomCommand = new LambdaCommand(e => MessageBox.Show("EXECUTED"), e => true);
+    }
+}
